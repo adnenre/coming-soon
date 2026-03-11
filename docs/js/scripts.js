05e3bfdf6608d18808fb50bbdeec12a41b200951
Adnen Rebai <adnen.rebai@gmail.com>
2026-03-11 06:55:12 +0100

    rename to docs

---
+++ docs/js/scripts.js
@@ -0,0 +1,35 @@
+
+$('document').ready(function(){
+  
+//scroll
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    }
+ 
+
+
+ 
+ });
